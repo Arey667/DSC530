5ffb7f196a6472da4adcd558aa53899c0a6e3609
Arey667 <54614954+Arey667@users.noreply.github.com>
2020-06-07 16:45:25 -0500

    Add files via upload

---
+++ AReyna_DSC530_Week1.4 assignment.py
@@ -0,0 +1,53 @@
+# Name: Arnold Reyna
+# Date: 6/6/2020
+# Course: DSC530
+# Assignment: 1.4  Assignment
+# Purpose: Perform the following operations:
+#------------------------------------------------------------------------
+#  1) Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print ('Hello World! I wonder why that is always the default coding text to start with')
+#  2) Add two numbers together
+num1 = 2
+num2 = 3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+#  3)Subtract a number from another number
+num1 = 2
+num2 = 3
+diff = num1 - num2
+print('The difference of {0} and {1} is {2}'.format(num1, num2, diff))
+
+#  4)Multiply two numbers
+num1 = 2
+num2 = 3
+mult = num1 * num2
+print('The product of {0} and {1} is {2}'.format(num1, num2, mult))
+
+#  5) Divide between two numbers
+num1 = 2
+num2 = 3
+div = num1 / num2
+print('The division of {0} and {1} is {2}'.format(num1, num2, div))
+
+#  6)Concatenate two strings together (any words)
+str1="Week "
+str2="One"
+print('string 1 is',str1)
+print('string 2 is',str2)
+str=str1+str2
+print("Concatenated two strings:",str)
+
+#  7)Create a list of 4 items (can be strings, numbers, both)
+my_list = ["month",6,"day",7,2020]
+print ('my_list',my_list)
+
+#  8)Append an item to your list (again, can be a string, number)
+my_list.append('date')
+print('adding the word date to my list is : ',my_list)
+
+#  9)Create a tuple with 4 items (can be strings, numbers, both)
+my_tuple = (1,5,'text',55.23)
+print('my tuple contains :',my_tuple)
+
+
+
